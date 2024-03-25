cad6be54bf6e7cc0d94016167c3be0953ff85536
Konik Rahman <konikrahman06@gmail.com>
2024-03-26 00:04:57 +0200

    jobtest.java: updated some information

--- JobTest.java
+++ JobTest.java
@@ -19,20 +19,15 @@ public class JobTest extends Application {
 
     @Override
     public void start(Stage stage) {
-
         stage.setTitle("Job name Interface"); // showing it in my window.
-
         Label jobNamelabel = new Label("Name of the Job:  ");
         this.nameTextField = new TextField();
 
         this.updateButton = new Button("Update"); // update button and passed it into nameBox
-
         this.nameTextField.setOnAction(this::nameProcesshandler); // event hanlder method
 
         // update button event action, same event as nameTextField.
-
         this.updateButton.setOnAction(this::nameProcesshandler);
-
         HBox nameBox = new HBox(
             jobNamelabel,
             this.nameTextField,
@@ -47,7 +42,6 @@ public class JobTest extends Application {
         // pane.getchildren- list of child added to the stackpane.
         //add(namebox)- adds the nameBow as a child of the stackpane.
         // the nameBox will be added to the StackPane as a child.
-
         // same result as like flowPane.
         StackPane pane = new StackPane();
         pane.getChildren().add(nameBox);
@@ -56,24 +50,17 @@ public class JobTest extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
     public void nameProcesshandler(ActionEvent event){
-
         String nameText = this.nameTextField.getText();
-
         if ( !nameText.isEmpty() && nameText.length() <= 20){ // checking textfield is not empty and length is less than 20 character.
-
             this.job = new Job(nameText);
             System.out.println(job);
-
             // clear the text field after clicking the update button.
             this.nameTextField.clear();
-
         } else{
             System.out.println("Text must not be empty! or not too long!.");
         }
     }
-
     public static void main(String[] args) {
         launch();
     }
